e4a8cf1566836121a924c06880a8c6aee40a5b66
meownoirsoft <156626321+meownoirsoft@users.noreply.github.com>
2024-12-13 20:39:53 -0700

    add domain in goto

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -17,7 +17,7 @@
         // Redirect to the home page after successful login
         successMessage = 'Login successful. Redirecting...';
         await new Promise((resolve) => setTimeout(resolve, 500)); // Add a short delay
-        goto('/');
+        goto(`${window.location.origin}/`);
       } else {
         const error = await response.json();
         errorMessage = error.error;
