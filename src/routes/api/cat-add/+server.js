2bea371839d84106dba23faa00ea42cebf58589f
meownoirsoft <156626321+meownoirsoft@users.noreply.github.com>
2024-12-14 22:15:47 -0700

    testing the api file paths

--- src/routes/api/cat-add/+server.js
+++ src/routes/api/cat-add/+server.js
@@ -1,5 +1,13 @@
 import { json } from '@sveltejs/kit';
 import { addRowToJson } from '$lib/helpers/jsonHelper.js';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename)
+console.log(__dirname)
 
 export async function POST({ request }) {
   try {
@@ -7,10 +15,15 @@ export async function POST({ request }) {
     const newRow = await request.json();
 
     // Path to the JSON file
-    const filePath = 'src/lib/data/cats.json';
+    const filePath = path.resolve(__dirname, '../../../lib/data/cats.json');
+    // const filePath = 'src/lib/data/cats.json';
 
-    // Add the new row
-    const updatedData = addRowToJson(filePath, newRow);
+    if (existsSync(filePath)) {
+      console.log('File exists:', filePath);
+      const updatedData = addRowToJson(filePath, newRow);
+    } else {
+        console.error('File does not exist:', filePath);
+    }
 
     // Respond with the updated data
     return json({ success: true, data: updatedData });
