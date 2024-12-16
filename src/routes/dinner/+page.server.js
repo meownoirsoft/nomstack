3f7140fe83591d72db5a226779b5ece3ff4493bf
meownoirsoft <156626321+meownoirsoft@users.noreply.github.com>
2024-12-16 16:31:40 -0700

    upd dinner file path del

--- src/routes/dinner/+page.server.js
+++ src/routes/dinner/+page.server.js
@@ -2,7 +2,6 @@ export async function load({url}) {
     const apiUrl = `${import.meta.env.VITE_BASE_URL}/api/meal-get?type=dinner`;
     const res = await fetch(apiUrl);
     const meals = await res.json();
-    const selsFilePath = path.resolve(__dirname,'../../../src/lib/data/selected.json');
     const selsRes = await fetch(`${import.meta.env.VITE_BASE_URL}/api/sels-get?type=dinner`);
     const sels = await selsRes.json();
     return { meals, sels: sels[0].meals.split(',').map(meal => parseInt(meal,10)) };
